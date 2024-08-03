0d93916edea7be7c88089c139f01f80e4dd5d5d0
SergDv1962 <dsv19621227@gmail.com>
2024-08-03 15:48:28 +0300

    Валидация ответа сервера (використання валідації з пакетом zod)

--- src/modules/users/api.ts
+++ src/modules/users/api.ts
@@ -1,15 +1,24 @@
 import { baseApi } from "../../shared/api";
 import { User, UserId } from "./users.slice";
+import { z } from 'zod';
+
+const UseDtoSchema = z.object({
+      id: z.string(),
+      name: z.string(),
+      description: z.string(),
+   })
 
 export const usersApi = baseApi.injectEndpoints({
    endpoints: (create) => ({
       getUsers: create.query<User[], void>({
          query: () => '/users',
          providesTags: ['Users'],
+         transformResponse: (res: unknown) => UseDtoSchema.array().parse(res),
       }),
       getUser: create.query<User, UserId>({
          query: (userId) => `/users/${userId}`,
          providesTags: ['Users'],
+         transformResponse: (res: unknown) => UseDtoSchema.parse(res),
       }),
       deleteUser: create.mutation<void, UserId>({
          query: (userId) => ({ method: 'DELETE', url: `./users/${userId}`}),
